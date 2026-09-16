b49cf56b088bbb83fffabfc5829cd9a5bd351f94
snjokall1nn <snjokallinn@snjokallinn.com>
2026-09-16 17:15:11 +0000

    Sync complete Þróttheima Hub experience

--- config.js
+++
@@ -1,2 +0,0 @@
-// Google Apps Script Web App endpoint for Þróttheimar Hub.
-window.THROTTHUB_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxTR7jB69ExOm9XgY3BFTB8kyO4WRdDg8inZMTTTrOqiBXXe8DTUwn3Y-oAnh-NRiOZ/exec';
