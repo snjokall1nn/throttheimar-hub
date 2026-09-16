1cfd9656620a1c6af7c84d69a91dc5518b482680
snjokall1nn <snjokallinn@snjokallinn.com>
2026-09-16 01:08:50 +0000

    Connect Þróttheimar Hub to Apps Script

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-// Paste the deployed Google Apps Script Web App URL here.
-// Example: window.THROTTHUB_SCRIPT_URL = 'https://script.google.com/macros/s/ABC123/exec';
-window.THROTTHUB_SCRIPT_URL = '';
+// Google Apps Script Web App endpoint for Þróttheimar Hub.
+window.THROTTHUB_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxTR7jB69ExOm9XgY3BFTB8kyO4WRdDg8inZMTTTrOqiBXXe8DTUwn3Y-oAnh-NRiOZ/exec';
