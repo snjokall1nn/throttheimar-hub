6f471ee607fdf5ce5440dda449af69f7ebc7ce31
snjokall1nn <snjokallinn@snjokallinn.com>
2026-09-16 00:47:58 +0000

    Add form endpoint config

---
+++ config.js
@@ -0,0 +1,3 @@
+// Paste the deployed Google Apps Script Web App URL here.
+// Example: window.THROTTHUB_SCRIPT_URL = 'https://script.google.com/macros/s/ABC123/exec';
+window.THROTTHUB_SCRIPT_URL = '';
